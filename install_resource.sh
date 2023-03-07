3dde67b845860d2b83e777c28313ba2feeddc94a
Theo2lt <tliot@student.42.fr>
2023-03-07 17:49:43 +0100

    debut wordpress

--- install_resource.sh
+++ install_resource.sh
@@ -1,6 +1,12 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
+sudo apt-get install curl -y
+sudo apt-get install gnupg -y
+sudo apt-get install ca-certificates -y
+sudo apt-get install lsb-release -y
+
 sudo apt-get install docker.io -y
 sudo apt-get install docker -y
+sudo apt-get install docker-compose-plugin -y
 sudo docker --version
 echo "FINISH"
