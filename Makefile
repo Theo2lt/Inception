b717c0beae54cfaa045ec4fc4afdee6b6b636edb
theo2lt <tliot@student.42.fr>
2023-04-18 23:35:48 +0000

    iwp-cli

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
 NAME = wordpress
 
 all:
+	sh install_resource.sh
+	sudo mkdir -p /home/tliot/data/db 
+	sudo mkdir -p /home/tliot/data/wordpress
+	sudo mkdir -p /home/tliot/data/minecraft-server
 	docker-compose -f ./srcs/docker-compose.yml up --build -d
 
 re :
@@ -11,9 +15,9 @@ down:
 
 
 clean:
-	docker rm -f $$(docker ps -qa);\
-	docker volume rm -f $$(docker volume ls);\
-	
+	docker rm -f $$(docker ps -qa)
+	docker volume rm -f $$(docker volume ls)
+	sudo rm -rf /home/tliot
 
 
 .PHONY: all clean fclean re
--- srcs/requirements/wordpress/conf/install.sh
+++ srcs/requirements/wordpress/conf/install.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
-FILE=wordpress
+FILE=wp-config.php
 cd /var/www/html
 
-if [ -d "$FILE" ]; then
-    echo "$FILE exists."
+if [ -f "$FILE" ]; then
+    echo "$FILE exists." > de
 else
-    echo "$FILE not exists."
-    wget https://wordpress.org/latest.zip;
-    unzip latest.zip
-    rm -rf latest.zip
-    cd $FILE
-    mv /home/wp-config.php .
-
-    sed -i -e "s/BDD_NAME/$BDD_WORDPRESS_NAME/g" wp-config.php
-    sed -i -e "s/BDD_USER/$BDD_WORDPRESS_USER/g" wp-config.php
-    sed -i -e "s/BDD_PASSWORD/$BDD_WORDPRESS_USER_PASSWORD/g" wp-config.php
-    sed -i -e "s/BDD_HOST/$HOST/g" wp-config.php
+    echo "$FILE not exists." > de
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    php wp-cli.phar --info
+    chmod +x wp-cli.phar
+    mv wp-cli.phar /usr/local/bin/wp
+    wp core download --allow-root
+    wp config create --dbname=$BDD_NAME --dbuser=$BDD_USER --dbpass=$BDD_USER_PASSWORD --dbhost=$BDD_HOST --allow-root
+    wp core install --url=$DOMAINE_NAME --title=INCEPTION --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
+    wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=$WP_USER_ROLE --porcelain --allow-root
 fi
 
-/usr/sbin/php-fpm7.3 -F
+/usr/sbin/php-fpm7.4 -F
